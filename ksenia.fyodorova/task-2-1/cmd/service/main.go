aab05cc9d2c6bbc0de67be4a15737aa8cf2c72ea
Ksenia <f.kcusha082@mail.ru>
2025-10-17 14:49:13 +0300

    [TASK-2-1] Fix lint

--- ksenia.fyodorova/task-2-1/cmd/service/main.go
+++ ksenia.fyodorova/task-2-1/cmd/service/main.go
@@ -8,46 +8,58 @@ import (
 	"strings"
 )
 
+const (
+	minTemp          = 15
+	maxTemp          = 30
+	minPartsLength   = 2
+	greaterEqualSign = ">="
+	lessEqualSign    = "<="
+)
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	scanner.Scan()
 	numOfDepartments, _ := strconv.Atoi(scanner.Text())
 
-	for i := 0; i < numOfDepartments; i++ {
+	for range numOfDepartments {
 		scanner.Scan()
 		numOfStaff, _ := strconv.Atoi(scanner.Text())
 
-		start := 15
-		end := 30
+		start := minTemp
+		end := maxTemp
 
-		for j := 0; j < numOfStaff; j++ {
+		for range numOfStaff {
 			scanner.Scan()
 			preference := scanner.Text()
 
 			parts := strings.Fields(preference)
-			if len(parts) < 2 {
+			if len(parts) < minPartsLength {
 				fmt.Println(-1)
+
 				continue
 			}
 
 			sign := parts[0]
 			temperature, err := strconv.Atoi(parts[1])
+
 			if err != nil {
 				fmt.Println(-1)
+
 				continue
 			}
 
-			if sign == ">=" {
+			if sign == greaterEqualSign {
 				if temperature > start {
 					start = temperature
 				}
-			} else if sign == "<=" {
+			} else if sign == lessEqualSign {
 				if temperature < end {
 					end = temperature
 				}
 			} else {
 				fmt.Println(-1)
+
 				continue
 			}
 
