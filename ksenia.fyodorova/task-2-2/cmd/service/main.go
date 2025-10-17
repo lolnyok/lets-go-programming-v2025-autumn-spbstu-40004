a9866775988c0c0fe84451de2a3b568cf3cd1d38
Ksenia <f.kcusha082@mail.ru>
2025-10-17 16:18:27 +0300

    [TASK-2-2] First normal version

--- ksenia.fyodorova/task-2-2/cmd/service/main.go
+++ ksenia.fyodorova/task-2-2/cmd/service/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
 func main() {
-	fmt.Println("TASK-2-2")
+	var n, k int
+	fmt.Scan(&n)
+
+	dishes := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&dishes[i])
+	}
+
+	fmt.Scan(&k)
+
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for _, dish := range dishes {
+		heap.Push(h, dish)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	result := heap.Pop(h).(int)
+	fmt.Println(result)
 }
