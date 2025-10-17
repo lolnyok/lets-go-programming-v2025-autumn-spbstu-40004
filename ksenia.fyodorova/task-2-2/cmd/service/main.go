c7a88ed2515790d729a2b54ab403bc2570abe27e
Ksenia <f.kcusha082@mail.ru>
2025-10-17 16:26:47 +0300

    [TASK-2-2] Fix all in lint

--- ksenia.fyodorova/task-2-2/cmd/service/main.go
+++ ksenia.fyodorova/task-2-2/cmd/service/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"log"
 )
 
 type DishHeap []int
 
-func (heap DishHeap) Len() int           { return len(heap) }
-func (heap DishHeap) Less(i, j int) bool { return heap[i] < heap[j] }
-func (heap DishHeap) Swap(i, j int)      { heap[i], heap[j] = heap[j], heap[i] }
+func (heap *DishHeap) Len() int           { return len(*heap) }
+func (heap *DishHeap) Less(i, j int) bool { return (*heap)[i] < (*heap)[j] }
+func (heap *DishHeap) Swap(i, j int)      { (*heap)[i], (*heap)[j] = (*heap)[j], (*heap)[i] }
 
 func (heap *DishHeap) Push(dish interface{}) {
-	*heap = append(*heap, dish.(int))
+	dishValue, ok := dish.(int)
+	if !ok {
+		log.Fatal("invalid type in Push")
+	}
+	*heap = append(*heap, dishValue)
 }
 
 func (heap *DishHeap) Pop() interface{} {
@@ -20,30 +25,46 @@ func (heap *DishHeap) Pop() interface{} {
 	lastIndex := len(oldHeap) - 1
 	dish := oldHeap[lastIndex]
 	*heap = oldHeap[:lastIndex]
+
 	return dish
 }
 
 func main() {
 	var totalDishes, preferenceRank int
-	fmt.Scan(&totalDishes)
-
-	dishRatings := make([]int, totalDishes)
-	for i := 0; i < totalDishes; i++ {
-		fmt.Scan(&dishRatings[i])
+	_, err := fmt.Scan(&totalDishes)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Scan(&preferenceRank)
+	dishRatings := make([]int, totalDishes)
+	for i := range totalDishes {
+		_, err := fmt.Scan(&dishRatings[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	_, err = fmt.Scan(&preferenceRank)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	dishHeap := &DishHeap{}
 	heap.Init(dishHeap)
 
 	for _, rating := range dishRatings {
 		heap.Push(dishHeap, rating)
+
 		if dishHeap.Len() > preferenceRank {
 			heap.Pop(dishHeap)
 		}
 	}
 
-	kthPreference := heap.Pop(dishHeap).(int)
+	result := heap.Pop(dishHeap)
+	kthPreference, ok := result.(int)
+	if !ok {
+		log.Fatal("invalid type from heap")
+	}
+
 	fmt.Println(kthPreference)
 }
