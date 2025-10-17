850a91b47239ee488bcf23b534fc2d16f056f869
Ksenia <f.kcusha082@mail.ru>
2025-10-17 16:22:05 +0300

    [TASK-2-2] Made more meaningful names for variables

--- ksenia.fyodorova/task-2-2/cmd/service/main.go
+++ ksenia.fyodorova/task-2-2/cmd/service/main.go
@@ -5,45 +5,45 @@ import (
 	"fmt"
 )
 
-type IntHeap []int
+type DishHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (heap DishHeap) Len() int           { return len(heap) }
+func (heap DishHeap) Less(i, j int) bool { return heap[i] < heap[j] }
+func (heap DishHeap) Swap(i, j int)      { heap[i], heap[j] = heap[j], heap[i] }
 
-func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+func (heap *DishHeap) Push(dish interface{}) {
+	*heap = append(*heap, dish.(int))
 }
 
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
+func (heap *DishHeap) Pop() interface{} {
+	oldHeap := *heap
+	lastIndex := len(oldHeap) - 1
+	dish := oldHeap[lastIndex]
+	*heap = oldHeap[:lastIndex]
+	return dish
 }
 
 func main() {
-	var n, k int
-	fmt.Scan(&n)
+	var totalDishes, preferenceRank int
+	fmt.Scan(&totalDishes)
 
-	dishes := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&dishes[i])
+	dishRatings := make([]int, totalDishes)
+	for i := 0; i < totalDishes; i++ {
+		fmt.Scan(&dishRatings[i])
 	}
 
-	fmt.Scan(&k)
+	fmt.Scan(&preferenceRank)
 
-	h := &IntHeap{}
-	heap.Init(h)
+	dishHeap := &DishHeap{}
+	heap.Init(dishHeap)
 
-	for _, dish := range dishes {
-		heap.Push(h, dish)
-		if h.Len() > k {
-			heap.Pop(h)
+	for _, rating := range dishRatings {
+		heap.Push(dishHeap, rating)
+		if dishHeap.Len() > preferenceRank {
+			heap.Pop(dishHeap)
 		}
 	}
 
-	result := heap.Pop(h).(int)
-	fmt.Println(result)
+	kthPreference := heap.Pop(dishHeap).(int)
+	fmt.Println(kthPreference)
 }
